fcb445b9972d7d30c0910b53afe4e8a651bad26b
Kevin Gonzales <83090260+kevin1go-tech@users.noreply.github.com>
2024-04-25 13:35:26 -0500

    Acomodando ofuscamiento

--- Banca web Ofuscado script.js
+++ Banca web Ofuscado script.js
@@ -1,1 +1,6 @@
-document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".captcha"),t=document.getElementById("captchaInput"),n=document.getElementById("captchaRefreshButton"),o=document.getElementById("submitButton"),r=document.getElementById("successModal"),d=document.getElementById("reloadButton"),a=document.getElementById("cardType"),c=document.getElementById("cardNumber"),l=document.getElementById("documentType"),u=document.getElementById("documentNumber"),s=document.getElementById("internetKey"),i=document.querySelector(".virtual-keyboard"),f=function(){const e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";let t="";for(let n=0;n
+document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".captcha"),
+t=document.getElementById("captchaInput"),n=document.getElementById("captchaRefreshButton"),o=document.getElementById("submitButton"),
+r=document.getElementById("successModal"),d=document.getElementById("reloadButton"),a=document.getElementById("cardType"),c=document.
+getElementById("cardNumber"),l=document.getElementById("documentType"),u=document.getElementById("documentNumber"),s=document.
+getElementById("internetKey"),i=document.querySelector(".virtual-keyboard"),f=function(){const e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq
+rstuvwxyz0123456789";let t="";for(let n=0;n
